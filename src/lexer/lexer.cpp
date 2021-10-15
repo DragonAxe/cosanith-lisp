37a46313cb7571a4955566d9f62a0d39a63f20e9
DragonAxe <DragonAxe@users.noreply.github.com>
2021-10-14 23:34:18 -0600

    Removed dead code in

--- src/lexer/lexer.cpp
+++ src/lexer/lexer.cpp
@@ -117,40 +117,6 @@ public:
     [[nodiscard]] virtual TokenType tokenType() const = 0;
 };
 
-// ///
-// /// 0x[0-9a-fA-F]+        6.hex number
-// class ScanHex : ScannerBase
-// {
-// private:
-//     std::function<char (bool)> matcherFn;
-// public:
-//     explicit ScanHex(std::function<char (bool)> fn) : matcherFn(fn)
-//     {}
-
-//     std::optional<lexer::Token> matchChar(char c) override
-//     {
-//         return std::nullopt;
-//     }
-// };
-
-// ///
-// /// 0[0-7]*               7.zero or octal number
-// class ScanOct : ScannerBase
-// {
-// private:
-//     std::vector<ScannerBase*> possibleExpressions;
-// public:
-//     explicit ScanOct(std::vector<ScannerBase*> possibleExpressions) : possibleExpressions(possibleExpressions)
-//     {}
-
-//     std::optional<lexer::Token> matchChar(char c) override
-//     {
-
-//         return std::nullopt;
-//     }
-// };
-
-
 /// ".*"                       String literal
 class ScanString : public ScannerBase {
 public:
@@ -472,251 +438,39 @@ public:
     }
 };
 
-
-//
-// Lexer states ---------------------------------------------------------------
-//
-
-///// Regex:
-///// \/\/.*                1.single line comment
-///// \/\*(.|\n)*\*\/       2.multi-line comment
-//Token lexComment(CharStream& in)
-//{
-//    using namespace std;
-//
-//    char c;
-//    bool wasStar = false;
-//    bool wasNewLine = false;
-//    stringstream out;
-//    Caret pos = in.pos();
-//
-//    out << in.get(); // First forward slash
-//
-//    switch (in.peek()) {
-//    case '/':
-//        out << in.get();
-//        getWhile([&wasNewLine](const char c){
-//            bool match = wasNewLine;
-//            wasNewLine = c == '\n';
-//            return !match; }, in, out);
-//        // while (true) {
-//        //     c = in.get();
-//        //     out << c;
-//        //     if (c == '\n' || in.eof()) { break; }
-//        // }
-//        break;
-//    case '*':
-//        out << in.get();
-//        while (true) {
-//            c = in.get();
-//            out << c;
-//            if ( (wasStar && c == '/') || in.eof() ) {
-//                break;
-//            }
-//            wasStar = c == '*';
-//        }
-//        break;
-//    }
-//
-//    return {TokenType::comment, out.str(), pos};
-//}
-//
-///// Regex:
-///// ".*?"                 3.string
-//Token lexString(CharStream& in)
-//{
-//    using namespace std;
-//
-//    bool wasBackslash = false;
-//    stringstream out;
-//    Caret pos = in.pos();
-//
-//    getExpect([](const char c) {
-//        return c == '"'; }, in, out);
-//
-//    getWhile([&wasBackslash](const char c) {
-//        bool match = !wasBackslash && c == '"';
-//        wasBackslash = c == '\\';
-//        return !match; }, in, out);
-//
-//    getExpect([](const char c) {
-//        return c == '"'; }, in, out);
-//
-//    return {TokenType::string, out.str(), pos};
-//}
-//
-///// Regex:
-///// '\\?.'                11.character literal
-//Token lexCharacter(CharStream& in)
-//{
-//    using namespace std;
-//
-//    char c;
-//    bool wasBackslash = false;
-//    stringstream out;
-//    Caret pos = in.pos();
-//
-//    out << in.get(); // first single quote
-//
-//    c = in.get();
-//    out << c;
-//    if (c == '\\') {
-//        out << in.get(); // get escaped char
-//    }
-//    out << in.get(); // ending single quote
-//
-//    if (in.eof()) {
-//        throw runtime_error("Unexpected EOF while scanning for character literal.");
-//    }
-//
-//    return {TokenType::character, out.str(), pos};
-//}
-//
-///// Regex:
-///// (                     4.left paren
-//Token lexLeftParen(CharStream& in)
-//{
-//    return {TokenType::leftParen, in.get(), in.pos()};
-//}
-//
-///// Regex:
-///// )                     5.right paren
-//Token lexRightParen(CharStream& in)
-//{
-//    return {TokenType::rightParen, in.get(), in.pos()};
-//}
-//
-///// Regex:
-///// 0                     12.zero
-///// 0x[0-9a-fA-F]+        6.hex number
-///// 0[0-7]+               7.octal number
-//Token lexZeroHexOctal(CharStream& in)
-//{
-//    using namespace std;
-//
-//    char c;
-//    stringstream out;
-//    Caret pos = in.pos();
-//
-//    out << in.get(); // prefix zero
-//
-//    c = in.get();
-//    if (in.eof() || !(isdigit(c) || c == 'x')) { // Zero
-//        return {TokenType::integer, '0', pos};
-//    } else if (c == 'x') { // Hex
-//        out << c; // x
-//        // There must be at least one digit after the x
-//        c = in.peek();
-//        if (!isHexDigit(c) || in.eof()) {
-//            throw runtime_error("For a hex value, there must be at least one valid hex digit after the '0x'.");
-//        }
-//        out << in.get(); // 1st digit
-//        while (true) {
-//            c = in.peek();
-//            if (isHexDigit(c) && !in.eof()) {
-//                out << in.get();
-//            } else {
-//                break;
-//            }
-//        }
-//        return {TokenType::integer, out.str(), pos};
-//    } else { // Octal
-//        throw runtime_error("Octal not implemented.");
-//    }
-//}
-//
-///// Regex:
-///// -?([1-9][0-9]*)?((\.?[0-9]+[df]?)|\.)     8.(+/-) int or float or double
-//Token lexNumber(CharStream& in)
-//{
-//    using namespace std;
-//
-//    char c;
-//    stringstream out;
-//
-//    c = in.peek();
-//    if (c == '-') {
-//        out << in.get(); // negative
-//        c = in.peek();
-//        if (!(isdigit(c) || c == '.')) {
-//            lexIdentifier(in, out.str());
-//        }
-//    }
-//    if (c == 0) {
-//        throw runtime_error("Number cannot start with zero");
-//    }
-//
-//
-//
-//
-//    return {TokenType::integer, "404", Caret()};
-//
-//    // stringstream out;
-//    // Caret pos = in.pos();
-//
-//    // char c = in.get();
-//    // if (c == '-') {
-//    //     c = in.get();
-//    //     if ( !isdigit(c) && !(c == '.') ) {
-//
-//    //     }
-//    // }
-//}
-//
-///// Regex:
-///// \s+                   10.whitespace
-//Token lexWhitespace(CharStream& in)
-//{
-//    using namespace std;
-//
-//    std::stringstream out;
-//    Caret pos = in.pos();
-//    char c;
-//
-//    while (true)
-//    {
-//        c = in.peek();
-//
-//        if (in.eof()) {
-//            goto no_more_whitespace;
-//        }
-//
-//        if (isspace(c)) {
-//            out << (char)in.get();
-//        } else {
-//            goto no_more_whitespace;
-//        }
-//    }
-//    no_more_whitespace:;
-//
-//    return {TokenType::whitespace, out.str(), pos};
-//}
-//
-///// Regex:
-///// [A-Za-z\+\-\*\/\!\@\#\$\%\^\&\*][A-Za-z0-9\+\-\*\/\!\@\#\$\%\^\&\*]*       9.identifier
-//Token lexIdentifier(CharStream& in)
-//{
-//    using namespace std;
-//
-//    stringstream out;
-//    Caret pos = in.pos();
-//
-//    while ( (isalnum(in.peek()) || (ispunct(in.peek()) && in.peek() != ')')) && !in.eof() ) {
-//        out << (char)in.get();
-//    }
-//
-//    string s = out.str();
-//
-//    if (s == "fn") {
-//        return {TokenType::keyword, s, pos};
-//    }
-//
-//    return {TokenType::literal, s, pos};
-//}
-
 } // anonymous namespace
 
 Token TokenStream::get() {
+    // Lexer regular language:
+    // \/\/.*|\/\*(.|\n)*\*\/|".*?"|'\\?.'|[()]|0x[0-9a-fA-F]+|0[0-7]0|-?([1-9][0-9]*)?((\.?[0-9]+[df]?)|\.)|[A-Za-z\+\-\*\/\!\@\#\$\%\^\&\*][A-Za-z0-9\+\-\*\/\!\@\#\$\%\^\&\*]*|\s+
+
+    // Possible cases:
+    // \/\/.*                1.single line comment
+    // \/\*(.|\n)*\*\/       2.multi-line comment
+    // ".*?"                 3.string
+    // '\\?.'                11.character literal
+    // (                     4.left paren
+    // )                     5.right paren
+    // 0                     12. zero
+    // 0x[0-9a-fA-F]+        6.hex number
+    // 0[0-7]*               7.zero or octal number
+    // -?([1-9][0-9]*)?((\.?[0-9]+[df]?)|\.)                                      8.(+/-) int or float or double
+    // \s+                   10.whitespace
+    // [A-Za-z\+\-\*\/\!\@\#\$\%\^\&\*][A-Za-z0-9\+\-\*\/\!\@\#\$\%\^\&\*]*       9.identifier
+
+    // First character to token map (state transition map)
+    // /    1|2  single or multi- line comment
+    // "    3    string
+    // '    11   character literal
+    // (    4    left paren
+    // )    5    right paren
+    // 0    6|7  zero or hex or octal number
+    // -    8    negative number
+    // 1-9  8    positive number
+    // .    8    floatingPoint number
+    //      10   whitespace
+    // !0-9 9    identifier
+
     using namespace std;
 
     if (mStreamStart) {
@@ -752,76 +506,6 @@ Token TokenStream::get() {
     } else {
         return {TokenType::error, std::string(1, c), pos};
     }
-
-
-    // throw runtime_error("WIP");
-
-    // Lexer regular language:
-    // \/\/.*|\/\*(.|\n)*\*\/|".*?"|'\\?.'|[()]|0x[0-9a-fA-F]+|0[0-7]0|-?([1-9][0-9]*)?((\.?[0-9]+[df]?)|\.)|[A-Za-z\+\-\*\/\!\@\#\$\%\^\&\*][A-Za-z0-9\+\-\*\/\!\@\#\$\%\^\&\*]*|\s+
-
-    // Possible cases:
-    // \/\/.*                1.single line comment
-    // \/\*(.|\n)*\*\/       2.multi-line comment
-    // ".*?"                 3.string
-    // '\\?.'                11.character literal
-    // (                     4.left paren
-    // )                     5.right paren
-    // 0                     12. zero
-    // 0x[0-9a-fA-F]+        6.hex number
-    // 0[0-7]*               7.zero or octal number
-    // -?([1-9][0-9]*)?((\.?[0-9]+[df]?)|\.)                                      8.(+/-) int or float or double
-    // \s+                   10.whitespace
-    // [A-Za-z\+\-\*\/\!\@\#\$\%\^\&\*][A-Za-z0-9\+\-\*\/\!\@\#\$\%\^\&\*]*       9.identifier
-
-    // First character to token map (state transition map)
-    // /    1|2  single or multi- line comment
-    // "    3    string
-    // '    11   character literal
-    // (    4    left paren
-    // )    5    right paren
-    // 0    6|7  zero or hex or octal number
-    // -    8    negative number
-    // 1-9  8    positive number
-    // .    8    floatingPoint number
-    //      10   whitespace
-    // !0-9 9    identifier
-
-
-    // if (mStreamStart) {
-    //     mStreamStart = false;
-    //     return Token(TokenType::start, "start", mIn->pos());
-    // }
-
-    // char c = mIn->peek();
-
-    // if (mIn->eof()) {
-    //     return Token(TokenType::eof, "eof", mIn->pos());
-    // }
-
-    // switch (c)
-    // {
-    // case '/':  return lexComment(*mIn);
-    // case '"':  return lexString(*mIn);
-    // case '\'': return lexCharacter(*mIn);
-    // case '(':  return lexLeftParen(*mIn);
-    // case ')':  return lexRightParen(*mIn);
-    // case '0':  return lexZeroHexOctal(*mIn);
-    // case '-':  return lexNumber(*mIn);
-    // case '.':  return lexNumber(*mIn);
-    // }
-
-    // if (isdigit(c)) { // [1-9], zero is captured earlier
-    //     return lexNumber(*mIn);
-    // }
-
-    // if (isspace(c)) {
-    //     return lexWhitespace(*mIn);
-    // }
-
-    // return lexIdentifier(*mIn);
-
-    // cout << "Lex error: Unrecognised character: \"" << c << "\"" << endl;
-    // throw runtime_error("Lex error: Unrecognised character");
 }
 
 namespace tests { // lexer::tests
