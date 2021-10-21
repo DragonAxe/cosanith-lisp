819e53834a6c4529e6b5f32a14bcaae9ee4d122f
DragonAxe <DragonAxe@users.noreply.github.com>
2021-10-21 01:09:20 -0600

    Reformat and added comments.

--- src/lexer/charStream.h
+++ src/lexer/charStream.h
@@ -5,72 +5,98 @@
 
 namespace lexer {
 
-/// Represents a file position.
+/// Represents a column and line position within a character stream.
 class Caret
 {
 public:
-    [[nodiscard]] long line() const {return mLine;}
-    [[nodiscard]] long col() const {return mCol;}
+  /// Returns the current line.
+  /// The first line in a file will return 1.
+  [[nodiscard]] long line() const { return mLine; }
 
-    void nextLine();
-    void nextChar();
+  /// Returns the current column.
+  /// New lines will restart the column position.
+  /// The first column on a line will return 1.
+  [[nodiscard]] long col() const { return mCol; }
 
-    std::string str();
+  /// Advances the caret's position to the next line.
+  /// Resets the column position.
+  void nextLine();
+
+  /// Advances the caret's position by one character.
+  void nextChar();
+
+  /// Return a string representation of ths position in the form line:col.
+  std::string str();
 
 private:
-    long mLine = 1;
-    long mCol = 1;
+  long mLine = 1;
+  long mCol = 1;
 };
 
+/// A virtual class representing a stream of characters from some source.
 class CharStream
 {
 public:
-    // Virtual methods:
+  // Virtual methods:
 
-    virtual char peek() = 0;
-    virtual char get() = 0;
-    [[nodiscard]] virtual bool eof() const = 0;
-    
-    // Concrete methods:
+  /// Look at the next character in the stream, but don't advance the stream's
+  /// position. Successive calls to peek will return the same character.
+  [[nodiscard]] virtual char peek() = 0;
 
-    [[nodiscard]] Caret pos() const { return mPos; }
+  /// Get the next character from the stream and advance the stream's position
+  /// to the next character.
+  [[nodiscard]] virtual char get() = 0;
+
+  /// Returns true if end of file has been reached. This function only reports
+  /// the stream state as set by the most recent I/O operation; it does not
+  /// examine the associated data source.
+  [[nodiscard]] virtual bool eof() const = 0;
+
+  // Concrete methods:
+
+  /// Get the stream's current position.
+  [[nodiscard]] Caret pos() const { return mPos; }
 
 protected:
-    Caret mPos;
+  Caret mPos;
 };
 
 /// A file stream wrapper which keeps track of line and column numbers in file.
 class FileCharStream : public CharStream
 {
 public:
-    /// Opens the given file for reading.
-    explicit FileCharStream(std::string  filename);
+  /// Opens the given file for reading.
+  explicit FileCharStream(std::string filename);
 
-    // Pass through methods to std::ifstream:
-    char peek() override;
-    char get() override;
-    bool eof() const override;
+  // Pass through methods to std::ifstream:
+  [[nodiscard]] char peek() override;
+
+  [[nodiscard]] char get() override;
+
+  [[nodiscard]] bool eof() const override;
 
 private:
-    const std::string mFilename;
-    std::ifstream mIn;
+  const std::string mFilename;
+  std::ifstream mIn;
 };
 
 /// A file stream wrapper which keeps track of line and column numbers in file.
 class StrCharStream : public CharStream
 {
 public:
-    /// Opens the given file for reading.
-    explicit StrCharStream(std::string  src);
+  /// Opens the given file for reading.
+  explicit StrCharStream(std::string src);
 
-    char peek() override;
-    char get() override;
-    [[nodiscard]] bool eof() const override;
+  [[nodiscard]] char peek() override;
+
+  [[nodiscard]] char get() override;
+
+  [[nodiscard]] bool eof() const override;
 
 private:
-    const std::string mSrc;
-    long mSrcPos = 0;
-    bool mEof = false;
+  const std::string mSrc;
+  long mSrcPos = 0;
+  bool mEof = false;
 };
 
 } // namespace lexer
